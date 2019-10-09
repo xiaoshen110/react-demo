39c5d8482d4a5b156de70fd527512387aec7932c
xiaoshen110 <xiaoshen110>
2019-10-09 17:10:27 +0800

    todolist

--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,8 @@ import { createStore } from 'redux'
 // 引入reducer 仓库管理员
 import reducer from './reducer'
 
-let store = createStore(reducer)
+let store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 export default store
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import { INPUT_CHANGE, ADD_LIST, DELETE_LIST } from './actionTypes'
+
 let defaultState = {
   inputValue: '请输入',
   lists: [
@@ -6,5 +8,27 @@ let defaultState = {
   ]
 }
 export default (state = defaultState, action) => {
+  console.log(state, action)
+  if (action.type === INPUT_CHANGE) {
+    // reducer里只能接收state, 不能直接修改 state, 
+    let newState = JSON.parse(JSON.stringify(state)) // 深度克隆
+    newState.inputValue = action.value
+    // 返回state的
+    return newState
+  }
+
+  if (action.type === ADD_LIST) {
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.lists.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+
+  if (action.type === DELETE_LIST) {
+    let newState = JSON.parse(JSON.stringify(state))
+    console.log('action.value', action.value)
+    newState.lists.splice(action.value)
+    return newState
+  }
   return state
 }
